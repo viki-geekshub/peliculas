affd65a40b4ba0c2a795ad53792eeef14365f369
viki <vikihp1982@hotmail.com>
2020-03-12 18:47:53 +0100

    Cambio de funcionalidad seleccionando el formulario para que funcione el botón

--- index.js
+++ index.js
@@ -21,8 +21,37 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820
 
 document.querySelector('.form-inline').addEventListener('submit',event=>event.preventDefault())
 
-// document.querySelector("#boton").addEventListener('click', event => {
-// fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='+event.target.value)
+document.querySelector('.buscarForm')
+.addEventListener('submit',event=>{
+  event.preventDefault();
+        fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='+event.target.buscarInput.value)
+        .then(res=>res.json())
+        .then(res=>{
+            const peliculas =res.results;
+            peliculasContainer.innerHTML='';
+            const baseImgUrl= 'https://image.tmdb.org/t/p/w185';
+            if(peliculas.length > 0){
+                peliculas.forEach(pelicula=>{
+                    const imagen = pelicula.poster_path ? `
+                    <img src="${baseImgUrl}${pelicula.poster_path}" alt="">`:'<h3 class="noimagen">No hay imagen disponible</h3>'
+                    peliculasContainer.innerHTML+=`
+                    <div class="pelicula">
+                    <h3 class="title">${pelicula.title}
+                    </h3>
+                    ${imagen}
+                    </div>`
+                })    
+            }else{
+                peliculasContainer.innerHTML += `<div class="nothing"><h2>No hay coincidencias</h2> </div>`
+            }
+                            
+        })
+})
+
+// document.querySelector("#boton").addEventListener('submit', event => {
+//     event.preventDefault();
+//     console.log("entra");
+//     fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='+event.target.buscarInput.value)
 // .then(res=>res.json())
 // .then(res=>{
 //     const peliculas =res.results;
@@ -46,51 +75,33 @@ document.querySelector('.form-inline').addEventListener('submit',event=>event.pr
     
 //     })
 // })
-    
-document.querySelector('.buscarInput')
-.addEventListener('keyup',event=>{
-    if(event.key === 'Enter'){
-        event.preventDefault();
-        fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='+event.target.value)
-        .then(res=>res.json())
-        .then(res=>{
-            const peliculas =res.results;
-            peliculasContainer.innerHTML='';
-            const baseImgUrl= 'https://image.tmdb.org/t/p/w185';
-            if( peliculas.length > 0){
-                peliculas.forEach(pelicula=>{
-                    const imagen = pelicula.poster_path ? `
-                    <img src="${baseImgUrl}${pelicula.poster_path}" alt="">`:'<h3 class="noimagen">No hay imagen disponible</h3>'
-                    
-                    peliculasContainer.innerHTML+=`
-                    <div class="pelicula">
-                <h3 class="title">${pelicula.title}
-                </h3>
-                ${imagen}
-            </div>`
-                })    
-            }else{
-                peliculasContainer.innerHTML += `<div class="nothing"><h2>No hay coincidencias</h2> </div>`
-            }
-            
-        })
-    }
 
-        // axios.get('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='+event.target.value)
-        // .then(res=>{
-        //     const peliculas =res.data.results;
-        //     peliculasContainer.innerHTML='';
-        //     const baseImgUrl= 'https://image.tmdb.org/t/p/w185';
-        //     peliculas.forEach(pelicula=>{
-        //         const imagen = pelicula.poster_path ? `
-        //         <img src="${baseImgUrl}${pelicula.poster_path}" alt="">`:''
-        //         peliculasContainer.innerHTML+=`
-        //         <div class="pelicula">
-        //             <h3 class="title">${pelicula.title}
-        //         </h3>
-        //         ${imagen}
-        //     </div>`
-        //     })
-        // })
-    }
-)
+// document.querySelector('.buscarInput')
+// .addEventListener('keyup',event=>{
+//     if(event.key === 'Enter'){
+//         event.preventDefault();
+//         fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='+event.target.value)
+//         .then(res=>res.json())
+//         .then(res=>{
+//             const peliculas =res.results;
+//             peliculasContainer.innerHTML='';
+//             const baseImgUrl= 'https://image.tmdb.org/t/p/w185';
+//             if( peliculas.length > 0){
+//                 peliculas.forEach(pelicula=>{
+//                     const imagen = pelicula.poster_path ? `
+//                     <img src="${baseImgUrl}${pelicula.poster_path}" alt="">`:'<h3 class="noimagen">No hay imagen disponible</h3>'
+                    
+//                     peliculasContainer.innerHTML+=`
+//                     <div class="pelicula">
+//                 <h3 class="title">${pelicula.title}
+//                 </h3>
+//                 ${imagen}
+//             </div>`
+//                 })    
+//             }else{
+//                 peliculasContainer.innerHTML += `<div class="nothing"><h2>No hay coincidencias</h2> </div>`
+//             }
+            
+//         })
+//     }
+
