20bd0d52f958a4d39d1ca2f3bee8c75998a6d060
viki <vikihp1982@hotmail.com>
2020-03-14 20:44:26 +0100

    Añadir condición para que no vaya a paginas negativas. No funciona del todo

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const peliculasContainer = document.querySelector('main.peliculas');
 const baseImgUrl = 'https://image.tmdb.org/t/p/w185';
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&page=1')
-    .then(res => res.json())
-    .then(res => {
-        const peliculas = res.results;
-        peliculasContainer.innerHTML = '';
-        
-        peliculas.forEach(pelicula => {
-            const imagen = pelicula.poster_path ? `
-                <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<h3 class="noimagen">No hay imagen disponible</h3>'
-            peliculasContainer.innerHTML += `
+
+const paginah3 = document.getElementById('pagina');
+let page = 1;
+const getPeliculasPopulares = (page) => {
+    if(page<1){
+        page=1
+    }else{
+        paginah3.innerText = page;
+
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&page=' + page)
+        .then(res => res.json())
+        .then(res => {
+            const peliculas = res.results;
+            peliculasContainer.innerHTML = '';
+            peliculas.forEach(pelicula => {
+                const imagen = pelicula.poster_path ? `
+                <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<h3 class="noimagen">No hay imagen disponible</h3>' 
+                peliculasContainer.innerHTML += `
                 <div class="pelicula" onclick="getPeliculaDetailed(${pelicula.id})">
                 <h3 class="title">${pelicula.title}
                 </h3>
                 ${imagen}
             </div>`
+            })
         })
-    })
-    .catch(error => console.error(error)) // No hace falta pero lo pongo por buenas prácticas.
+        .catch(error => console.error(error)) // No hace falta pero lo pongo por buenas prácticas.
+    }
+}
+getPeliculasPopulares(page);
 
 document.querySelector('.form-inline').addEventListener('submit', event => event.preventDefault())
 
@@ -51,12 +62,12 @@ document.querySelector('.buscarForm')
         }
     })
 
-const getPeliculaDetailed = movie_id=>{
+const getPeliculaDetailed = movie_id => {
     axios.get(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`)
-    .then(res=>{
-        const pelicula = res.data;
-        if(pelicula.overview !== ''){
-            peliculasContainer.innerHTML = `
+        .then(res => {
+            const pelicula = res.data;
+            if (pelicula.overview !== '') {
+                peliculasContainer.innerHTML = `
             <div class="details">
                 <h3 class="title">${pelicula.title}</h3>
                 <div class="imageDetails">
@@ -64,8 +75,8 @@ const getPeliculaDetailed = movie_id=>{
                     <p>${pelicula.overview}</p>
                 </div>
             </div>`
-        }else{
-            peliculasContainer.innerHTML = `
+            } else {
+                peliculasContainer.innerHTML = `
             <div class="details">
                 <h3 class="title">${pelicula.title}</h3>
                 <div class="imageDetails">
@@ -73,9 +84,9 @@ const getPeliculaDetailed = movie_id=>{
                     <p><h3 class="noimagen nodescripcion">No hay descripción disponible</h3></p>
                 </div>
             </div>`
-        }
-       
-    })
+            }
+
+        })
 }
 
 // OTRA FORMA DE HACERLO - PERO NO FUNCIONA EL BOTON
